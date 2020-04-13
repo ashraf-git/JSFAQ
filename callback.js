67b6b95080fb4d86b8746437a267d0e72ec2d580
ashraf-git <aliashraf.hena@gmail.com>
2020-04-13 11:41:31 +0600

    12_5-3 Callback function and pass different function

---
+++ callback.js
@@ -0,0 +1,18 @@
+function explain_callback(name, age, task){
+    console.log('Hello ', name);
+    console.log('your age ', age);
+    task();
+}
+function washHand(){
+    console.log('wash hand with soap')
+}
+function takeShower(){
+    console.log('take your bath');
+}
+function scrollFacebook(){
+    console.log('Scroll facebook but dont like any post');
+}
+explain_callback('Sogir Uddin', 17, washHand);
+explain_callback('Jogir Uddin', 13, takeShower);
+explain_callback('Mugir Uddin', 13, scrollFacebook);
+
